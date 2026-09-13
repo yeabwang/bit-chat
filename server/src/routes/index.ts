ad435f41cff0398a13845de7c5a21de3036d1997
Vagif693 <bavaqif2007@gmail.com>
2026-09-13 20:26:22 +0800

    feat: mount friend routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import authRoutes from "./auth.route";
 import conversationRoutes from "./conversation.route";
+import friendRoutes from "./friend.route";
 import userRoutes from "./user.route";
 
 const router = Router();
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/conversations", conversationRoutes);
+router.use("/friends", friendRoutes);
 
 export default router;
